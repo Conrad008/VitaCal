cd5900b81bc74891b0c6585c801fd8a84fd83eeb
Conrad008 <kipngeno7180@gmail.com>
2026-06-07 17:17:56 +0300

    feat: store foodstuff in local storage using unique id

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ const resetBtn = document.getElementById('reset-btn');
 const emptyState = document.getElementById('empty-state');
 const fetchSuggestionBtn = document.getElementById('suggestion-btn');
 const suggestionDisplay = document.getElementById('suggestion-display');
+
+const updateAppState = () => {
+    localStorage.setItem('VitaCal_foods', JSON.stringify(foodItems));
+    render();
+};
+
+/**
+ * Handles adding a new item to the data array.
+ * @param {string} name - Name of the food item
+ * @param {number} calories - Amount of calories
+ */
+const addFoodItem = (name, calories) => {
+    let uniqueId;
+    if (crypto.randomUUID){
+        uniqueId = crypto.randomUUID();
+    }
+
+    else {
+        uniqueId = Date.now().toString();
+    }
+    const newItem = {
+        id: uniqueId,
+        name: name.trim(),
+        calories: parseInt(calories, 10)
+    };
+    foodItems.push(newItem);
+    updateAppState();
+};
