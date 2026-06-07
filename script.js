7b47aa13f119130895ff9f5a9a5b1b673b04d8f0
Conrad008 <kipngeno7180@gmail.com>
2026-06-07 17:00:06 +0300

    declare element selectors

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const fooditems = [];
 
+const foodForm = document.getElementById('food-form');
+const foodNameInput = document.getElementById('food-name');
+const calorieInput = document.getElementById('calorie-amount');
+const foodList = document.getElementById('food-list');
+const totalCaloriesDisplay = document.getElementById('total-calories');
+const resetBtn = document.getElementById('reset-btn');
+const emptyState = document.getElementById('empty-state');
+const fetchSuggestionBtn = document.getElementById('suggestion-btn');
+const suggestionDisplay = document.getElementById('suggestion-display');
